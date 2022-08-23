b60c31fef461ad5cb5247492e502731301abcf35
Eon Slemp <eonslemp@gmail.com>
2022-08-23 15:17:22 -0400

    methods revisted answered

--- review/2jsreview.js
+++ review/2jsreview.js
@@ -25,6 +25,8 @@ const dart = ["d'art", 'demogorgan dog', 2, 'upside down']
 
 
 plantee[2]++;
+// console.log(plantee.findIndex(attribute => Number.isInteger(attribute) === true))
+// plantee.every(attribute => {(Number.isInteger(attribute) === true)++})
 // console.log(plantee)
 
 wolfy.splice(wolfy.findIndex(attribute => attribute == 'yukon territory'), 1,'gotham city')
@@ -39,7 +41,31 @@ dart[3].push(home, 'hawkins')
 wolfy.splice(0,1, 'gameboy')
 // console.log(wolfy)
 
-let tmnt = ['donatello','leonardo', 'raphael', 'michaelangelo']
-for(let turtle of tmnt){
-    console.log(turtle.toUpperCase())
-}
+// let tmnt = ['donatello','leonardo', 'raphael', 'michaelangelo']
+// for(let turtle of tmnt){
+//     console.log(turtle.toUpperCase())
+// }
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained',
+ 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic',
+  'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther',
+   'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious']
+//  console.log(favMovies)
+// console.log(favMovies.length)
+// console.log(favMovies.findIndex(movie => movie === 'Titanic'))
+favMovies.sort()
+// console.log(favMovies)
+// 1. yes sort altered the order of the array
+favMovies.pop()
+favMovies.push('Gaurdians of the Galaxy')
+// console.log(favMovies)
+favMovies.reverse()
+// console.log(favMovies)
+console.log(favMovies.shift())
+console.log(favMovies.unshift('gaurdians of the galaxy'))
+// the unshift returns the length of the mutated array
+
+// favMovies.splice(favMovies.findIndex(movie => movie === "Django Unchained"), 1, 'Avatar')
+// console.log(favMovies)
+let backHalf = favMovies.slice(favMovies.length/2, favMovies.length)
+// console.log(backHalf)
+// slice pulled out a subset of the array that is composed of the last half of the array
