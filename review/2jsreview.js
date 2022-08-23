581c373e51756409df43b41858742649024edbc1
Eon Slemp <eonslemp@gmail.com>
2022-08-23 14:02:37 -0400

    fizzbuzz answered

--- review/2jsreview.js
+++ review/2jsreview.js
@@ -1,7 +1,19 @@
-//  part 2 js reps
+//  part 2 js reps easy going 
 // for (i = 0; i <= 20; i++){
 //     console.log(i)
 // }
-for(let i = 0; i <=200; i+=2){
-    console.log(i)
+// get even
+// for(let i = 0; i <=200; i+=2){
+//     console.log(i)
+// }
+
+// fizz buzz
+for(let i = 0; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log('fizz buzz')
+    }else if(i % 5 == 0){
+        console.log('buzz')
+    }else if(i % 3 == 0){
+        console.log('fizz')
+    }else{ console.log(i)}
 }
